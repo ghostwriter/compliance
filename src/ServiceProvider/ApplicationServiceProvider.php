c1542d429059273a8892264d0ab72908aea20abb
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-20 17:43:38 -0400

    Create ApplicationServiceProvider.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/ServiceProvider/ApplicationServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\AutomatedCompliance\ServiceProvider;
+
+use Composer\InstalledVersions;
+use Ghostwriter\Container\Contract\ContainerInterface;
+use Ghostwriter\Container\Contract\ServiceProviderInterface;
+use Ghostwriter\EventDispatcher\Contract\DispatcherInterface;
+use Ghostwriter\EventDispatcher\Dispatcher;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
+
+final class ApplicationServiceProvider implements ServiceProviderInterface
+{
+    public function __invoke(ContainerInterface $container): void
+    {
+        $container->bind(DispatcherInterface::class, Dispatcher::class);
+
+        $container->set('app.name', 'Automated Compliance Test');
+        $container->set('app.package', InstalledVersions::getRootPackage()['name'] ?? 'ghostwriter/automated-compliance');
+        $container->set('app.version', InstalledVersions::getPrettyVersion($container->get('app.package')));
+
+        $container->extend(
+            Application::class,
+            static function ($container, $application): object {
+                /** @var Application $application */
+                $application->setAutoExit(false);
+                $application->setCatchExceptions(false);
+
+                foreach ($container->tagged(Command::class) as $command) {
+                    $application->add($container->get($command));
+                }
+
+                return $application;
+            }
+        );
+    }
+}
