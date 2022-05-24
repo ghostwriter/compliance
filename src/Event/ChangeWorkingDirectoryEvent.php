b4c66fbc6217ae8e1b7a8e3d84d26aea4f167447
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-23 22:47:53 -0400

    Update ChangeWorkingDirectoryEvent.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Event/ChangeWorkingDirectoryEvent.php
+++ src/Event/ChangeWorkingDirectoryEvent.php
@@ -7,7 +7,6 @@ namespace Ghostwriter\Compliance\Event;
 use Ghostwriter\Compliance\Command\ChangeWorkingDirectoryCommand;
 use Ghostwriter\EventDispatcher\Contract\DispatcherInterface;
 use Symfony\Component\Console\Input\ArrayInput;
-use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Throwable;
 use function getcwd;
@@ -17,7 +16,11 @@ final class ChangeWorkingDirectoryEvent extends AbstractEvent
     /**
      * @throws Throwable
      */
-    public function __construct(DispatcherInterface $dispatcher, ChangeWorkingDirectoryCommand $command, SymfonyStyle $output)
+    public function __construct(
+        DispatcherInterface $dispatcher,
+        ChangeWorkingDirectoryCommand $command,
+        SymfonyStyle $output
+    )
     {
         $input = new ArrayInput([
             '--current-working-directory' => getcwd(),
