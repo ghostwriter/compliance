aa86f7f0d1c01908c48d2955a0afcea6c8c9bb2f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-25 16:22:20 -0400

    Create CheckCommand.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Command/CheckCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Compliance\Command;
+
+use Ghostwriter\Compliance\Event\GenerateWorkflowFileEvent;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Throwable;
+
+final class CheckCommand extends AbstractCommand
+{
+    protected function configure(): void
+    {
+        $this->setDescription('Performs the QA job');
+        $this->addArgument('job', InputArgument::REQUIRED, 'JSON string representing the job to run.');
+    }
+
+    /**
+     * Execute the command.
+     *
+     * @throws Throwable
+     *
+     * @return int 0 if everything went fine, or an exit code
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        /** @var GenerateWorkflowFileEvent $generateWorkflowFileEvent */
+        $generateWorkflowFileEvent =  $this->dispatcher->dispatch(
+            new CheckEvent($this->dispatcher, $input, $this->output)
+        );
+
+        return $generateWorkflowFileEvent->isPropagationStopped() ? self::FAILURE : self::SUCCESS;
+    }
+}
