cb9274f55b97404f67cf4af3685630ca9d058b71
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-25 16:19:40 -0400

    Create Composer.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Tool/Composer.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Compliance\Tool;
+
+use Ghostwriter\Compliance\Contract\PresenceInterface;
+use const DIRECTORY_SEPARATOR;
+use function basename;
+use function getenv;
+use function trim;
+
+final class Composer extends AbstractTool implements PresenceInterface
+{
+    public const PRESENCE_FILES = ['composer.json'];
+
+    /**
+     * Retrieve the path to composer.json file.
+     */
+    public function getJsonFilePath(string $root): string
+    {
+        return $root . DIRECTORY_SEPARATOR . basename(trim(getenv('COMPOSER') ?: 'composer.json'));
+    }
+
+    /**
+     * Retrieve the path to composer.lock file.
+     */
+    public function getLockFilePath(string $root): string
+    {
+        $composerJsonPath = $this->getJsonFilePath($root);
+
+        return 'json' === pathinfo($composerJsonPath, PATHINFO_EXTENSION)
+            ? substr($composerJsonPath, 0, -4) . 'lock'
+            : $composerJsonPath . '.lock';
+    }
+}
