65d3b7b9ed0a748268b9aae63b20d95a9c9c1f27
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-25 21:11:33 -0400

    Update PresenceInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Contract/PresenceInterface.php
+++ src/Contract/PresenceInterface.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\Contract;
 
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Finder\Finder;
 
 interface PresenceInterface
 {
-    public function __construct(Finder $finder);
+    public function __construct(Finder $finder, SymfonyStyle $output);
 
     public function isPresent(): bool;
 }
