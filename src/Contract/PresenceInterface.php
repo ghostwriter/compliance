5716b69f4d8c6011cc6ab4646284ca0977057e53
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-26 10:57:09 -0400

    Update PresenceInterface.php

--- src/Contract/PresenceInterface.php
+++ src/Contract/PresenceInterface.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\Contract;
 
-use Symfony\Component\Finder\Finder;
-
 interface PresenceInterface
 {
     public function isPresent(): bool;
