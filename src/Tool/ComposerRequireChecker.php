3e074ed032c6fdd79b796a4b20f78b547f7ca870
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-26 10:42:52 -0400

    Update ComposerRequireChecker.php

--- src/Tool/ComposerRequireChecker.php
+++ src/Tool/ComposerRequireChecker.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\Tool;
 
-use Ghostwriter\Compliance\Contract\PresenceInterface;
-
-final class ComposerRequireChecker extends AbstractTool implements PresenceInterface
+final class ComposerRequireChecker extends AbstractTool
 {
     public const PRESENCE_FILES = ['composer-require-checker.json'];
 }
