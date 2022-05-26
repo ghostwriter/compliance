25bf7305c13affa47b4349f7c052f8a84262d511
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-26 10:48:15 -0400

    Update AbstractTool.php

--- src/Tool/AbstractTool.php
+++ src/Tool/AbstractTool.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Ghostwriter\Compliance\Tool;
 
 use Ghostwriter\Compliance\Contract\PresenceInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Finder\Finder;
 use function getcwd;
 use function getenv;
@@ -18,7 +19,7 @@ abstract class AbstractTool implements PresenceInterface
      */
     public const PRESENCE_FILES = [];
 
-    public function __construct(private Finder $finder)
+    public function __construct(private Finder $finder, private SymfonyStyle $output)
     {
     }
 
@@ -32,6 +33,13 @@ abstract class AbstractTool implements PresenceInterface
             ->in($path)
             ->depth(0)
             ->sortByName();
+
+        $this->output->section($path);
+//        $this->output->writeln(scandir($path));
+//        /** @var SplFileInfo $file */
+//        foreach ($finder->getIterator() as $file){
+//            $this->output->writeln($file->getPathname());
+//        }
 
         return $finder->name(static::PRESENCE_FILES)->hasResults();
     }
