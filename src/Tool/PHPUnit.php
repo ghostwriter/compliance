fbceb0d817a6dd3cab434a7baacf675485effb05
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-26 10:43:09 -0400

    Update PHPUnit.php

--- src/Tool/PHPUnit.php
+++ src/Tool/PHPUnit.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\Tool;
 
-use Ghostwriter\Compliance\Contract\PresenceInterface;
-
-final class PHPUnit extends AbstractTool implements PresenceInterface
+final class PHPUnit extends AbstractTool
 {
     public const PRESENCE_FILES = ['phpunit.xml.dist', 'phpunit.xml'];
 }
