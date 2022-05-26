f068390103ee3ab8c2cede36284e9341fc1e17ad
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-26 09:37:50 -0400

    Update GenerateMatrixEvent.php

--- src/Event/GenerateMatrixEvent.php
+++ src/Event/GenerateMatrixEvent.php
@@ -6,4 +6,22 @@ namespace Ghostwriter\Compliance\Event;
 
 final class GenerateMatrixEvent extends AbstractEvent
 {
+    private array $matrix;
+
+    /**
+     * @return array
+     */
+    public function getMatrix(): array
+    {
+        return $this->matrix;
+    }
+
+    /**
+     * @param array $matrix
+     */
+    public function setMatrix(array $matrix): void
+    {
+        $this->matrix[] = $matrix;
+    }
+
 }
