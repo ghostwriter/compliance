35b686c71a22d71222ead76ed274a50b77f4789f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-26 10:43:07 -0400

    Update PHPCS.php

--- src/Tool/PHPCS.php
+++ src/Tool/PHPCS.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\Tool;
 
-use Ghostwriter\Compliance\Contract\PresenceInterface;
-
-final class PHPCS extends AbstractTool implements PresenceInterface
+final class PHPCS extends AbstractTool
 {
     public const PRESENCE_FILES = ['phpcs.xml.dist', 'phpcs.xml'];
 }
