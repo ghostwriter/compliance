2c0ff029f2c0f637b33a90548479aa98c4c0f42b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-27 09:52:02 -0400

    Create ToolInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Contract/ToolInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Compliance\Contract;
+
+interface ToolInterface
+{
+    public function command(): string;
+
+    public function isPresent(): bool;
+
+    public function name(): string;
+}
