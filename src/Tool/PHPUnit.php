6c7008c68aa225737fbee583c498c37a0c0f7660
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-27 09:53:58 -0400

    Update PHPUnit.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Tool/PHPUnit.php
+++ src/Tool/PHPUnit.php
@@ -7,4 +7,9 @@ namespace Ghostwriter\Compliance\Tool;
 final class PHPUnit extends AbstractTool
 {
     public const PRESENCE_FILES = ['phpunit.xml.dist', 'phpunit.xml'];
+
+    public function command(): string
+    {
+        return './vendor/bin/phpunit';
+    }
 }
