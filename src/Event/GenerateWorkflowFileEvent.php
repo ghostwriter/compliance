f6f51a3e043841560c1e6fae7e8889a6cd540b32
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-27 22:48:26 -0400

    Update GenerateWorkflowFileEvent.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Event/GenerateWorkflowFileEvent.php
+++ src/Event/GenerateWorkflowFileEvent.php
@@ -4,10 +4,6 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\Event;
 
-/**
- * @template TPropagationStopped of bool
- * @extends AbstractEvent<TPropagationStopped>
- */
 final class GenerateWorkflowFileEvent extends AbstractEvent
 {
 }
