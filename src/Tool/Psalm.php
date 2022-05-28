95e692b912617e3727b8db4ac290df85fb428e02
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-27 21:58:55 -0400

    Update Psalm.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Tool/Psalm.php
+++ src/Tool/Psalm.php
@@ -6,10 +6,13 @@ namespace Ghostwriter\Compliance\Tool;
 
 final class Psalm extends AbstractTool
 {
-    public const PRESENCE_FILES = ['psalm.xml.dist', 'psalm.xml'];
-
     public function command(): string
     {
         return './vendor/bin/psalm --shepherd --stats --output-format=github --no-cache';
+    }
+
+    public function configuration(): array
+    {
+        return ['psalm.xml.dist', 'psalm.xml'];
     }
 }
