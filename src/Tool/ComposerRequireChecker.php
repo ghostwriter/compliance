f40b006c8684c628e94e44d1c68c8865cc956214
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-27 21:56:31 -0400

    Update ComposerRequireChecker.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Tool/ComposerRequireChecker.php
+++ src/Tool/ComposerRequireChecker.php
@@ -6,10 +6,13 @@ namespace Ghostwriter\Compliance\Tool;
 
 final class ComposerRequireChecker extends AbstractTool
 {
-    public const PRESENCE_FILES = ['composer-require-checker.json'];
-
     public function command(): string
     {
         return './vendor/bin/composer-require-checker check --config-file=composer-require-checker.json -n -v composer.json';
+    }
+
+    public function configuration(): array
+    {
+        return ['composer-require-checker.json'];
     }
 }
