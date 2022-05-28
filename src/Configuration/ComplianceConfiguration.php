2760200e77a981a69627ad35113457147d575b8f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-27 23:16:08 -0400

    Update ComplianceConfiguration.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Configuration/ComplianceConfiguration.php
+++ src/Configuration/ComplianceConfiguration.php
@@ -5,22 +5,14 @@ declare(strict_types=1);
 namespace Ghostwriter\Compliance\Configuration;
 
 use Ghostwriter\Compliance\ValueObject\ComposerDependency;
-use Ghostwriter\Compliance\ValueObject\PhpVersion;
 use Ghostwriter\Container\Contract\ContainerInterface;
 use RuntimeException;
 use function array_key_exists;
 
 final class ComplianceConfiguration
 {
-    private ContainerInterface $container;
-
-    private array $exclude = [];
-
-    private array $include = [];
-
-    public function __construct(ContainerInterface $container)
+    public function __construct(private ContainerInterface $container)
     {
-        $this->container = $container;
     }
 
     public function composerDependency(string $dependency): void
@@ -40,24 +32,6 @@ final class ComplianceConfiguration
         $this->container->set(ComposerDependency::CONFIG . '.options', $options);
     }
 
-    public function getExclude(): array
-    {
-        return $this->exclude;
-    }
-
-    public function getInclude(): array
-    {
-        return $this->include;
-    }
-
-    public function paths(array $array): void
-    {
-        /** @var string $value */
-        foreach ($array as $value) {
-            $this->include[] = $value;
-        }
-    }
-
     public function phpVersion(int $phpVersion): void
     {
         $this->container->set(ComposerDependency::CONFIG . '.php', $phpVersion);
@@ -65,9 +39,5 @@ final class ComplianceConfiguration
 
     public function skip(array $array): void
     {
-        /** @var string $value */
-        foreach ($array as $value) {
-            $this->exclude[] = $value;
-        }
     }
 }
