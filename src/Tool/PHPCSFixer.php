61bdafe72d244af0529c1d12a1fdfe2abe670205
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-27 21:58:42 -0400

    Update PHPCSFixer.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Tool/PHPCSFixer.php
+++ src/Tool/PHPCSFixer.php
@@ -6,10 +6,13 @@ namespace Ghostwriter\Compliance\Tool;
 
 final class PHPCSFixer extends AbstractTool
 {
-    public const PRESENCE_FILES = ['.php-cs-fixer.php', '.php-cs-fixer.dist.php'];
-
     public function command(): string
     {
         return './vendor/bin/php-cs-fixer';
+    }
+
+    public function configuration(): array
+    {
+        return ['.php-cs-fixer.php', '.php-cs-fixer.dist.php'];
     }
 }
