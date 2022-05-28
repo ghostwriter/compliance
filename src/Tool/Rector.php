c46c6619528c99933204776145b79902db47ee4f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-27 21:59:00 -0400

    Update Rector.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Tool/Rector.php
+++ src/Tool/Rector.php
@@ -6,10 +6,13 @@ namespace Ghostwriter\Compliance\Tool;
 
 final class Rector extends AbstractTool
 {
-    public const PRESENCE_FILES = ['rector.php'];
-
     public function command(): string
     {
         return './vendor/bin/rector process';
+    }
+
+    public function configuration(): array
+    {
+        return ['rector.php'];
     }
 }
