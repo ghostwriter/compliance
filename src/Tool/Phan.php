97c57c39533ce334a200e6ee7b037ddc3d137948
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-27 21:57:33 -0400

    Update Phan.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Tool/Phan.php
+++ src/Tool/Phan.php
@@ -6,10 +6,13 @@ namespace Ghostwriter\Compliance\Tool;
 
 final class Phan extends AbstractTool
 {
-    public const PRESENCE_FILES = ['.phan/config.php'];
-
     public function command(): string
     {
         return './vendor/bin/phan';
+    }
+
+    public function configuration(): array
+    {
+        return ['.phan/config.php'];
     }
 }
