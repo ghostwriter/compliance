c2c2110484d16fc2246ee7fb664f29fddb649e0c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-27 21:56:04 -0400

    Update Codeception.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Tool/Codeception.php
+++ src/Tool/Codeception.php
@@ -6,10 +6,13 @@ namespace Ghostwriter\Compliance\Tool;
 
 final class Codeception extends AbstractTool
 {
-    public const PRESENCE_FILES = ['codeception.yml.dist', 'codeception.yml'];
-
     public function command(): string
     {
         return './vendor/bin/codecept run';
+    }
+
+    public function configuration(): array
+    {
+        return ['codeception.yml.dist', 'codeception.yml'];
     }
 }
