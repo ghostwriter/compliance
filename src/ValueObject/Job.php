fdcd031a5aa19d72e081402940a2b40f27f29c0b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-05-27 21:59:38 -0400

    Update Job.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/ValueObject/Job.php
+++ src/ValueObject/Job.php
@@ -6,4 +6,25 @@ namespace Ghostwriter\Compliance\ValueObject;
 
 final class Job
 {
+    public function __construct(
+        private string $name,
+        private string $command,
+        private array $dependencies,
+        private int $php = PhpVersion::CURRENT_STABLE,
+        private bool $experimental =false,
+        private string $os = 'ubuntu-latest'
+    ) {
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'name' => $this->name,
+            'command' => $this->command,
+            'os' => $this->os,
+            'php' => PhpVersion::TO_STRING[$this->php],
+            'dependencies'=>$this->dependencies,
+            'experimental'=> $this->experimental,
+        ];
+    }
 }
