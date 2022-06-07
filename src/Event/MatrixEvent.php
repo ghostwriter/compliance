3c3f0dd9514313308159367654b1951b53a35486
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-06-06 23:56:11 -0400

    Create MatrixEvent.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Event/MatrixEvent.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Compliance\Event;
+
+use Ghostwriter\Compliance\ValueObject\Job;
+use JsonException;
+use function json_encode;
+
+final class MatrixEvent extends AbstractEvent
+{
+    /**
+     * @var array{
+     *     exclude:list<string>,
+     *     include:list<array{
+     *     name:string,
+     *     command:string,
+     *     os:string,
+     *     php:string,
+     *     dependency:string,
+     *     experimental:bool
+     * }>,
+     * }
+     */
+    private array $matrix = [
+        'include' => [],
+        'exclude' => [],
+    ];
+
+    public function exclude(array $matrices): void
+    {
+        /** @var string $matrix */
+        foreach ($matrices as $matrix) {
+            $this->matrix['exclude'][] = $matrix;
+        }
+    }
+
+    /**
+     * @throws JsonException
+     */
+    public function getMatrix(): string
+    {
+        return json_encode($this->matrix, JSON_THROW_ON_ERROR);
+    }
+
+    public function include(Job $job): void
+    {
+        $this->matrix['include'][] = $job->toArray();
+    }
+}
