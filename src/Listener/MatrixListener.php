5a74c77b74f4cfd6b3964493368af110efddc240
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-06-06 23:56:32 -0400

    Create MatrixListener.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Listener/MatrixListener.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Compliance\Listener;
+
+use Ghostwriter\Compliance\Contract\EventListenerInterface;
+use Ghostwriter\Compliance\Contract\ToolInterface;
+use Ghostwriter\Compliance\Event\MatrixEvent;
+use Ghostwriter\Compliance\ValueObject\ComposerDependency;
+use Ghostwriter\Compliance\ValueObject\Job;
+use Ghostwriter\Compliance\ValueObject\Tool;
+use Ghostwriter\Container\Container;
+use Throwable;
+
+final class MatrixListener implements EventListenerInterface
+{
+    /**
+     * @var string[]
+     */
+    private const DEPENDENCIES = ['latest', 'locked', 'lowest'];
+
+    public function __construct(private Container $container)
+    {
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function __invoke(MatrixEvent $generateMatrixEvent): void
+    {
+        foreach ($this->container->tagged(Tool::class) as $traversable) {
+            /** @var ToolInterface $tool */
+            $tool = $this->container->get($traversable);
+            if ($tool->isPresent()) {
+                /** @var int $phpVersion */
+                $phpVersion = $this->container->get(ComposerDependency::CONFIG . '.php');
+                foreach (self::DEPENDENCIES as $dependency) {
+                    // Todo: support including/excluding $dependency
+                    if ('latest' === $dependency) {
+                        $generateMatrixEvent->include(
+                            new Job($tool->name(), $tool->command(), $dependency, $phpVersion)
+                        );
+                    }
+                }
+            }
+        }
+    }
+}
