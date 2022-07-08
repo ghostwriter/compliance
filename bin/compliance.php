41932acec7071429a2bfce5f4cb0a4606182c4b5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-07-08 12:32:31 -0500

    Update compliance.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- bin/compliance.php
+++ bin/compliance.php
@@ -5,19 +5,19 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance;
 
-use Ghostwriter\Container\Container;
 use function dirname;
 use function sprintf;
 
-/** @var null|string $_composer_autoload_path */
+/** @var ?string $_composer_autoload_path */
 (static function (string $composerAutoloadPath): void {
     /** @psalm-suppress UnresolvableInclude */
     require $composerAutoloadPath ?: fwrite(
         STDERR,
         sprintf('[ERROR]Cannot locate "%s"\n please run "composer install"\n', $composerAutoloadPath)
     ) && exit(1);
+
     /**
      * #BlackLivesMatter.
      */
-    Compliance::main(Container::getInstance());
+    Compliance::main();
 })($_composer_autoload_path ?? dirname(__DIR__) . '/vendor/autoload.php');
