2f07a78b0e1a344643afcbb3d9703ea9a08ca3cc
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-01-17 01:33:03 -0600

    Update AbstractEvent.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Event/AbstractEvent.php
+++ src/Event/AbstractEvent.php
@@ -4,14 +4,17 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\Event;
 
-use Ghostwriter\EventDispatcher\AbstractEvent as EventDispatcherAbstractEvent;
 use Ghostwriter\EventDispatcher\Contract\DispatcherInterface;
+use Ghostwriter\EventDispatcher\Contract\EventInterface;
+use Ghostwriter\EventDispatcher\Traits\EventTrait;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Throwable;
 
-abstract class AbstractEvent extends EventDispatcherAbstractEvent
+abstract class AbstractEvent implements EventInterface
 {
+    use EventTrait;
+
     /**
      * @throws Throwable
      */
