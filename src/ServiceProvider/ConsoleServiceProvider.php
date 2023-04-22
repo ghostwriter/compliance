706e8bd0eeb83c36288ad104bb01a0ff3969e0bb
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-04-22 15:02:04 -0500

    Update ConsoleServiceProvider.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/ServiceProvider/ConsoleServiceProvider.php
+++ src/ServiceProvider/ConsoleServiceProvider.php
@@ -15,16 +15,12 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Formatter\OutputFormatter;
 use Symfony\Component\Console\Formatter\OutputFormatterInterface;
 use Symfony\Component\Console\Input\ArgvInput;
-use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\Input;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Output\ConsoleOutput;
-use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\Console\Output\Output;
 use Symfony\Component\Console\Output\OutputInterface;
 
-use Symfony\Component\Console\Style\SymfonyStyle;
 use function dirname;
 use function preg_match;
 use function sprintf;
