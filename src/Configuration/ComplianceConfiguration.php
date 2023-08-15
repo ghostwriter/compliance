ec7d2534a0964d1e92777c5de8606e993ad22edb
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-08-15 16:38:03 -0500

    Update ComplianceConfiguration.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Configuration/ComplianceConfiguration.php
+++ src/Configuration/ComplianceConfiguration.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\Configuration;
 
+use Ghostwriter\Compliance\Contract\ToolInterface;
 use Ghostwriter\Compliance\Option\ComposerDependency;
 use Ghostwriter\Compliance\Option\PhpVersion;
 use Ghostwriter\Container\ContainerInterface;
@@ -15,7 +16,7 @@ final class ComplianceConfiguration
     public function __construct(
         private ContainerInterface $container
     ) {
-        $this->container->set(ComposerDependency::CONFIG . '.php', PhpVersion::CURRENT_LATEST);
+        $this->container->set(ComposerDependency::CONFIG . '.php', PhpVersion::LATEST);
     }
 
     public function composerDependency(string $dependency): void
@@ -35,12 +36,22 @@ final class ComplianceConfiguration
         $this->container->set(ComposerDependency::CONFIG . '.options', $options);
     }
 
+    /**
+     * @param array<int|array<class-string<ToolInterface>,list<int>> $array
+     */
+    public function configure(array $array): void
+    {
+    }
+
     public function phpVersion(int $phpVersion): void
     {
         $this->container->remove(ComposerDependency::CONFIG . '.php');
         $this->container->set(ComposerDependency::CONFIG . '.php', $phpVersion);
     }
 
+    /**
+     * @param array<int|array<class-string<ToolInterface>,list<int>> $array
+     */
     public function skip(array $array): void
     {
     }
