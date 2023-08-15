0f57525a2a921df6c88bdc471270f1c6bd4351b3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-08-15 18:19:12 -0500

    Update ComplianceConfiguration.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Configuration/ComplianceConfiguration.php
+++ src/Configuration/ComplianceConfiguration.php
@@ -19,25 +19,8 @@ final class ComplianceConfiguration
         $this->container->set(ComposerDependency::CONFIG . '.php', PhpVersion::LATEST);
     }
 
-    public function composerDependency(string $dependency): void
-    {
-        if (! array_key_exists($dependency, ComposerDependency::OPTIONS)) {
-            throw new RuntimeException();
-        }
-
-        $this->container->set(ComposerDependency::CONFIG . '.dependency', $dependency);
-    }
-
     /**
-     * @param array<ComposerDependency::*> $options
-     */
-    public function composerOptions(array $options): void
-    {
-        $this->container->set(ComposerDependency::CONFIG . '.options', $options);
-    }
-
-    /**
-     * @param array<class-string<ToolInterface>,array<int,list<string>>> $checks
+     * @param array<class-string<ToolInterface>,array<int,list<string>>>                       $checks
      * @param array<class-string<ToolInterface>|int|string,array<int,list<string>>|int|string> $skips
      */
     public function __invoke(array $checks, array $skips): void
@@ -74,6 +57,23 @@ final class ComplianceConfiguration
                 }
             }
         }
+    }
+
+    public function composerDependency(string $dependency): void
+    {
+        if (! array_key_exists($dependency, ComposerDependency::OPTIONS)) {
+            throw new RuntimeException();
+        }
+
+        $this->container->set(ComposerDependency::CONFIG . '.dependency', $dependency);
+    }
+
+    /**
+     * @param array<ComposerDependency::*> $options
+     */
+    public function composerOptions(array $options): void
+    {
+        $this->container->set(ComposerDependency::CONFIG . '.options', $options);
     }
 
     private function getKey(string $tool, int $phpVersion, string $dependency): string
