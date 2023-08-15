c2eadc59595abad4fe8922ad6ac39d42d16156f5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-08-15 16:24:06 -0500

    Create Infection.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Tool/Infection.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Compliance\Tool;
+
+final class Infection extends AbstractTool
+{
+    public function command(): string
+    {
+        return 'composer infection || true';
+    }
+
+    /**
+     * @return string[]
+     */
+    public function configuration(): array
+    {
+        return ['infection.json5', 'infection.json', 'infection.json.dist', 'infection.json5.dist'];
+    }
+}
