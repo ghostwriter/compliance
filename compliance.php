a8d2579b773eb35626e54016772247cdfabf1f38
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-08-19 12:35:38 -0500

    Update compliance.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- compliance.php
+++ compliance.php
@@ -6,26 +6,22 @@ use Ghostwriter\Compliance\Configuration\ComplianceConfiguration;
 use Ghostwriter\Compliance\Option\ComposerDependency;
 use Ghostwriter\Compliance\Option\PhpVersion;
 use Ghostwriter\Compliance\Tool\ComposerRequireChecker;
-use Ghostwriter\Compliance\Tool\ECS;
 use Ghostwriter\Compliance\Tool\Infection;
 use Ghostwriter\Compliance\Tool\PHPBench;
 use Ghostwriter\Compliance\Tool\PHPCS;
 use Ghostwriter\Compliance\Tool\PHPCSFixer;
 use Ghostwriter\Compliance\Tool\PHPUnit;
 use Ghostwriter\Compliance\Tool\Psalm;
-use Ghostwriter\Compliance\Tool\Rector;
 
 return static function (ComplianceConfiguration $complianceConfiguration): void {
     ($complianceConfiguration)([
         ComposerRequireChecker::class => [PhpVersion::STABLE => [ComposerDependency::LOCKED]],
-        ECS::class => [PhpVersion::STABLE => [ComposerDependency::LOCKED]],
         Infection::class => [PhpVersion::ANY => ComposerDependency::ALL],
         PHPBench::class => [PhpVersion::ANY => ComposerDependency::ALL],
         PHPCS::class => [PhpVersion::STABLE => [ComposerDependency::LOCKED]],
         PHPCSFixer::class => [PhpVersion::STABLE => [ComposerDependency::LOCKED]],
         PHPUnit::class => [PhpVersion::ANY => ComposerDependency::ALL],
         Psalm::class => [PhpVersion::LATEST => [ComposerDependency::LOCKED]],
-        Rector::class => [PhpVersion::LATEST => [ComposerDependency::LOCKED]],
     ],
     [
         PhpVersion::DEV,
@@ -38,7 +34,6 @@ return static function (ComplianceConfiguration $complianceConfiguration): void 
             ]
         ],
         ComposerRequireChecker::class => [PhpVersion::STABLE => [ComposerDependency::LOCKED]],
-        Rector::class => [PhpVersion::LATEST => [ComposerDependency::LOCKED]],
         Infection::class => [PhpVersion::LATEST => [ComposerDependency::LOCKED]],
     ]);
 };
