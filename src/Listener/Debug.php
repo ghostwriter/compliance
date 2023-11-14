7976eb92c25d35251e55ff67d46e5d866f7d3062
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-11-14 17:05:45 -0600

    Update Debug.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Listener/Debug.php
+++ src/Listener/Debug.php
@@ -5,16 +5,17 @@ declare(strict_types=1);
 namespace Ghostwriter\Compliance\Listener;
 
 use Ghostwriter\Compliance\Contract\EventListenerInterface;
+use Ghostwriter\EventDispatcher\Interface\EventInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-final class Debug implements EventListenerInterface
+final readonly class Debug implements EventListenerInterface
 {
     public function __construct(
-        private readonly SymfonyStyle $symfonyStyle
+        private SymfonyStyle $symfonyStyle
     ) {
     }
 
-    public function __invoke(object $event): void
+    public function __invoke(EventInterface $event): void
     {
         $this->symfonyStyle->section($event::class);
     }
