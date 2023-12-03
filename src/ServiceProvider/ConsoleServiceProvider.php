49bbaa1225869122250e2dae32875a45efad1562
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-12-03 00:00:51 -0600

    Update ConsoleServiceProvider.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/ServiceProvider/ConsoleServiceProvider.php
+++ src/ServiceProvider/ConsoleServiceProvider.php
@@ -4,65 +4,30 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\ServiceProvider;
 
-use Ghostwriter\Compliance\Compliance;
-use Ghostwriter\Container\ContainerInterface;
-use Ghostwriter\Container\ServiceProviderInterface;
-use RecursiveDirectoryIterator;
-use RecursiveIteratorIterator;
-use SplFileInfo;
-use Symfony\Component\Console\Application as SymfonyApplication;
-use Symfony\Component\Console\Command\Command;
+use Ghostwriter\Compliance\Extension\SymfonyApplicationExtension;
+use Ghostwriter\Compliance\Factory\SymfonyApplicationFactory;
+use Ghostwriter\Container\Interface\ContainerInterface;
+use Ghostwriter\Container\Interface\ServiceProviderInterface;
+use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Formatter\OutputFormatter;
 use Symfony\Component\Console\Formatter\OutputFormatterInterface;
 use Symfony\Component\Console\Input\ArgvInput;
-use Symfony\Component\Console\Input\Input;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\ConsoleOutput;
-use Symfony\Component\Console\Output\Output;
 use Symfony\Component\Console\Output\OutputInterface;
+use Throwable;
 
-use function dirname;
-use function preg_match;
-use function sprintf;
-use function str_replace;
-
-final class ConsoleServiceProvider implements ServiceProviderInterface
+final readonly class ConsoleServiceProvider implements ServiceProviderInterface
 {
+    /**
+     * @throws Throwable
+     */
     public function __invoke(ContainerInterface $container): void
     {
-        $container->alias(SymfonyApplication::class, Compliance::class);
-        // Input
-        $container->set(ArgvInput::class, new ArgvInput());
-        $container->alias(Input::class, ArgvInput::class);
-        $container->alias(InputInterface::class, Input::class);
-        // Output
+        $container->alias(InputInterface::class, ArgvInput::class);
+        $container->alias(OutputInterface::class, ConsoleOutput::class);
         $container->alias(OutputFormatterInterface::class, OutputFormatter::class);
-        $container->alias(Output::class, ConsoleOutput::class);
-        $container->alias(OutputInterface::class, Output::class);
-        // Commands
-        /** @var SplFileInfo $splFileInfo */
-        foreach (
-            new RecursiveIteratorIterator(
-                new RecursiveDirectoryIterator(dirname(__DIR__) . '/Command/')
-            ) as $splFileInfo
-        ) {
-            $path = $splFileInfo->getPathname();
-
-            if (! preg_match('#Command\.php$#', $path)) {
-                continue;
-            }
-
-            if (str_contains(mb_strtolower($path), 'abstract')) {
-                continue;
-            }
-
-            $command = sprintf(
-                '%s%s',
-                str_replace('ServiceProvider', 'Command', __NAMESPACE__ . '\\'),
-                $splFileInfo->getBasename('.php')
-            );
-
-            $container->bind($command, $command, [Command::class]);
-        }
+        $container->extend(Application::class, SymfonyApplicationExtension::class);
+        $container->factory(Application::class, SymfonyApplicationFactory::class);
     }
 }
