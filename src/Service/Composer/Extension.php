a1f7638b60252d67e62a1ecb79a24eeecec012f8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-12-03 00:06:19 -0600

    Create Extension.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Service/Composer/Extension.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Compliance\Service\Composer;
+
+use Ghostwriter\Json\Json;
+
+final readonly class Extension implements \JsonSerializable, \Stringable, Dependency
+{
+    public function __construct(
+        private DependencyName $name,
+        private DependencyVersion $version
+    ) {
+    }
+
+    public function name(): DependencyName
+    {
+        return $this->name;
+    }
+
+    public function version(): DependencyVersion
+    {
+        return $this->version;
+    }
+
+    public function __toString(): string
+    {
+        return mb_substr((string) $this->name, 4);
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [ (string)  $this => (string) $this->version];
+    }
+}
+
+
+
+
