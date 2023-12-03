81637e8c4b956874a87dc8bc9430ade380c76c5a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-12-03 00:00:20 -0600

    Update MatrixServiceProvider.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/ServiceProvider/MatrixServiceProvider.php
+++ src/ServiceProvider/MatrixServiceProvider.php
@@ -4,41 +4,46 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\ServiceProvider;
 
+use Ghostwriter\Compliance\Service\Finder;
 use Ghostwriter\Compliance\ToolInterface;
-use Ghostwriter\Container\ContainerInterface;
-use Ghostwriter\Container\ServiceProviderInterface;
-
-use Symfony\Component\Finder\Finder;
+use Ghostwriter\Container\Interface\ContainerInterface;
+use Ghostwriter\Container\Interface\ServiceProviderInterface;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
 use function dirname;
 use function sprintf;
+use function str_contains;
+use function str_ends_with;
 use function str_replace;
 
-final class MatrixServiceProvider implements ServiceProviderInterface
+final readonly class MatrixServiceProvider implements ServiceProviderInterface
 {
     public function __construct(
-        private readonly Finder $finder
+        private Finder $finder,
     ) {
     }
 
     public function __invoke(ContainerInterface $container): void
     {
-        $finder = clone $this->finder;
+        $files = $this->finder->findIn(dirname(__DIR__) . '/Tool/');
+        foreach ($files as $file) {
+            $path = $file->getPathname();
 
-        $finder->files()
-            ->in(dirname(__DIR__) . '/Tool/')
-            ->notName('Abstract*.php')
-            ->sortByName();
+            if (str_contains($path, 'Abstract') || !str_ends_with($path, '.php')) {
+                continue;
+            }
 
-        foreach ($finder->getIterator() as $splFileInfo) {
-            $container->bind(
-                sprintf(
-                    '%s%s',
-                    str_replace('ServiceProvider', 'Tool', __NAMESPACE__ . '\\'),
-                    $splFileInfo->getBasename('.php')
+            $service = sprintf(
+                '%s%s',
+                str_replace(
+                    'ServiceProvider',
+                    'Tool',
+                    __NAMESPACE__ . '\\'
                 ),
-                null,
-                [ToolInterface::class]
+                $file->getBasename('.php')
             );
+
+            $container->tag($service, [ToolInterface::class]);
         }
     }
 }
