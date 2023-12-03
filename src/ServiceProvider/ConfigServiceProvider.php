f5be6cb5434fe7b211a12bbf66f0aa050dd250b6
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-12-03 00:01:21 -0600

    Create ConfigServiceProvider.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/ServiceProvider/ConfigServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Compliance\ServiceProvider;
+
+use Ghostwriter\Compliance\Extension\ConfigExtension;
+use Ghostwriter\Config\Config;
+use Ghostwriter\Config\ConfigFactory;
+use Ghostwriter\Config\Contract\ConfigFactoryInterface;
+use Ghostwriter\Config\Contract\ConfigInterface;
+use Ghostwriter\Container\Interface\ContainerInterface;
+use Ghostwriter\Container\Interface\ExceptionInterface;
+use Ghostwriter\Container\Interface\ServiceProviderInterface;
+
+final readonly class ConfigServiceProvider implements ServiceProviderInterface
+{
+    /**
+     * @throws ExceptionInterface
+     */
+    public function __invoke(ContainerInterface $container): void
+    {
+        $container->alias(ConfigInterface::class, Config::class);
+        $container->alias(ConfigFactoryInterface::class, ConfigFactory::class);
+        $container->extend(Config::class, ConfigExtension::class);
+    }
+}
