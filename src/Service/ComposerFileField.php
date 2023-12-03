6ef3382d46e3aa23c2a548c6bec498606744eefe
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-12-03 00:02:09 -0600

    Create ComposerFileField.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Service/ComposerFileField.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Compliance\Service;
+
+
+final readonly class ComposerFileField
+{
+    /**
+     * @var string
+     */
+    public const NAME = 'name';
+    /**
+     * @var string
+     */
+    public const DESCRIPTION = 'description';
+    /**
+     * @var string
+     */
+    public const VERSION = 'version';
+    /**
+     * @var string
+     */
+    public const LICENSE = 'license';
+    /**
+     * @var string
+     */
+    public const REQUIRE = 'require';
+}
