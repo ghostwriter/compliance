f9849b3d8877d9a350572c43a897ea18f274ca0a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-12-03 10:22:20 -0600

    Create ComposerCacheFilesDirectoryFinder.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Option/ComposerCacheFilesDirectoryFinder.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Compliance\Option;
+
+use Ghostwriter\Compliance\Service\Process;
+
+final readonly class ComposerCacheFilesDirectoryFinder
+{
+    public function __construct(
+        private Process $process = new Process()
+    ) {
+    }
+
+    public function find(): string
+    {
+        [$stdout, $stderr] = $this->process->execute(['composer', 'config', 'cache-files-dir']);
+
+        if (trim($stderr) !== '') {
+            throw new \RuntimeException(sprintf(
+                'Could not find composer cache files directory: %s%s',
+                PHP_EOL,
+                $stderr
+            ));
+        }
+
+        $composerCacheFilesDirectory = trim($stdout);
+
+        if ('' === $composerCacheFilesDirectory) {
+            $composerCacheFilesDirectory = implode(
+                DIRECTORY_SEPARATOR,
+                [getenv('HOME') ?: realpath('~/'), '.composer', 'cache', 'files']
+            );
+        }
+
+        return $composerCacheFilesDirectory;
+    }
+}
