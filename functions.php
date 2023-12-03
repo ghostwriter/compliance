dd79ad2f6235012edf9874d40c2986b7317bf21f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-12-03 00:39:47 -0600

    Update functions.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- functions.php
+++ functions.php
@@ -2,7 +2,9 @@
 
 declare(strict_types=1);
 
+use Ghostwriter\Compliance\Compliance;
 use Ghostwriter\Compliance\Event\OutputEvent;
+use Ghostwriter\Compliance\Service\GithubActionOutput;
 use Ghostwriter\Container\Container;
 use Ghostwriter\Container\Interface\ContainerInterface;
 use Ghostwriter\EventDispatcher\Interface\DispatcherInterface;
@@ -18,7 +20,35 @@ function dispatch(EventInterface $event): EventInterface
         ->get(DispatcherInterface::class)
         ->dispatch($event);
 }
-function dispatchOutputEvent(string ...$message): OutputEvent
+function dispatchOutputEvent(string|array $message): OutputEvent
 {
-   return dispatch(new OutputEvent(...$message));
+   return dispatch(new OutputEvent(
+       [
+           '::echo::on',
+           sprintf('::group::%s %s', Compliance::NAME, Compliance::BLACK_LIVES_MATTER),
+           $message,
+           '::endgroup::',
+           '::echo::off'
+       ]
+   ));
+}
+function githubActionOutput(): GithubActionOutput
+{
+   return container()
+       ->get(GithubActionOutput::class);
+}
+function debug(string $message, ?string $file = null, ?int $line = null, ?int $col = null): void
+{
+    githubActionOutput()
+        ->debug($message, $file, $line, $col);
+}
+function warning(string $message, ?string $file = null, ?int $line = null, ?int $col = null): void
+{
+    githubActionOutput()
+        ->warning($message, $file, $line, $col);
+}
+function error(string $message, ?string $file = null, ?int $line = null, ?int $col = null): void
+{
+    githubActionOutput()
+        ->error($message, $file, $line, $col);
 }
