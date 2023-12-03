36549f12c05ed1eb0e81973228be3d4db2b05298
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-12-03 00:37:52 -0600

    Create RequirePhp.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Service/RequirePhp.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Compliance\Service;
+
+final readonly class RequirePhp implements PhpVersionConstraint
+{
+    public function __construct(
+        private string $version,
+    ) {
+    }
+
+    public function getVersion(): string
+    {
+        return $this->version;
+    }
+}
