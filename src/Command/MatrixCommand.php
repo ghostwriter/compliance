9d6269debf11d7e2388e6cac786be3e65462c466
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-12-03 00:32:03 -0600

    Update MatrixCommand.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Command/MatrixCommand.php
+++ src/Command/MatrixCommand.php
@@ -6,18 +6,20 @@ namespace Ghostwriter\Compliance\Command;
 
 use Ghostwriter\Compliance\Compliance;
 use Ghostwriter\Compliance\Event\MatrixEvent;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Throwable;
-use const PHP_EOL;
-use function sprintf;
 
+#[AsCommand(
+    name: 'matrix',
+    description: 'Generates a job matrix for Github Actions.',
+)]
 final class MatrixCommand extends AbstractCommand
 {
     protected function configure(): void
     {
-        $this->setDescription('Generates a job matrix for Github Actions.');
         $this->addArgument('job', InputArgument::OPTIONAL, 'JSON string representing the job to run.');
     }
 
