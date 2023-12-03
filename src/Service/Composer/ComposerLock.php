12f4886e032b4094804ad3af881dcc7d21598dd8
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-12-03 00:05:35 -0600

    Create ComposerLock.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Service/Composer/ComposerLock.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Compliance\Service\Composer;
+
+final readonly class ComposerLock {
+    /**
+     * @param array<int,mixed> $contents
+     */
+    public function __construct(
+        private string $path,
+        private array $contents,
+    ) {
+    }
+
+    public function getComposerJsonPath(): string
+    {
+        return $this->path;
+    }
+    /**
+     * @return array<int,mixed>
+     */
+    public function getContents(): array
+    {
+        return $this->contents;
+    }
+}
