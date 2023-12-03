e18af7b8257584f8397ad3804dbdb4535b593573
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-12-03 14:28:01 -0600

    Update ApplicationServiceProvider.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/ServiceProvider/ApplicationServiceProvider.php
+++ src/ServiceProvider/ApplicationServiceProvider.php
@@ -8,6 +8,8 @@ use Ghostwriter\Compliance\Extension\ComplianceExtension;
 use Ghostwriter\Container\Interface\ContainerInterface;
 use Ghostwriter\Container\Interface\ServiceProviderInterface;
 use Throwable;
+use Ghostwriter\Compliance\Option\ComposerExecutableFinder;
+use Ghostwriter\Compliance\Factory\ComposerExecutableFinderFactory;
 
 final readonly class ApplicationServiceProvider implements ServiceProviderInterface
 {
@@ -20,5 +22,6 @@ final readonly class ApplicationServiceProvider implements ServiceProviderInterf
         $container->provide(EventServiceProvider::class);
         $container->provide(ConfigServiceProvider::class);
         $container->provide(MatrixServiceProvider::class);
+        $container->factory(ComposerExecutableFinder::class, ComposerExecutableFinderFactory::class);
     }
 }
