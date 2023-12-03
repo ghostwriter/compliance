8496502dafbcdf2832cd654288f0acb5762f8f3f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-12-03 00:31:55 -0600

    Update CheckCommand.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Command/CheckCommand.php
+++ src/Command/CheckCommand.php
@@ -5,16 +5,20 @@ declare(strict_types=1);
 namespace Ghostwriter\Compliance\Command;
 
 use Ghostwriter\Compliance\Event\CheckEvent;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Throwable;
 
+#[AsCommand(
+    name: 'check',
+    description: 'Performs the QA job',
+)]
 final class CheckCommand extends AbstractCommand
 {
     protected function configure(): void
     {
-        $this->setDescription('Performs the QA job');
         $this->addArgument('job', InputArgument::REQUIRED, 'JSON string representing the job to run.');
     }
 
