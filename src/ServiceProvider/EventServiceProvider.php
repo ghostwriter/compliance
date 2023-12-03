070144fd75bb34408ab31916caeb64702b6efde4
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-12-03 00:00:33 -0600

    Update EventServiceProvider.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/ServiceProvider/EventServiceProvider.php
+++ src/ServiceProvider/EventServiceProvider.php
@@ -4,61 +4,25 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\ServiceProvider;
 
-use Ghostwriter\Container\ContainerInterface;
-use Ghostwriter\Container\ExceptionInterface;
-use Ghostwriter\Container\ServiceProviderInterface;
-use Ghostwriter\EventDispatcher\Contract\DispatcherInterface;
-use Ghostwriter\EventDispatcher\Contract\EventInterface;
-use Ghostwriter\EventDispatcher\Contract\ListenerProviderInterface;
+use Ghostwriter\Compliance\Extension\ListenerProviderExtension;
+use Ghostwriter\Container\Interface\ContainerInterface;
+use Ghostwriter\Container\Interface\ExceptionInterface;
+use Ghostwriter\Container\Interface\ServiceProviderInterface;
+use Ghostwriter\EventDispatcher\Interface\DispatcherInterface;
+use Ghostwriter\EventDispatcher\Interface\ListenerProviderInterface;
 use Ghostwriter\EventDispatcher\Dispatcher;
 use Ghostwriter\EventDispatcher\ListenerProvider;
-use Symfony\Component\Finder\Finder;
-use function dirname;
-use function sprintf;
-use function str_replace;
+use Throwable;
 
-final class EventServiceProvider implements ServiceProviderInterface
+final readonly class EventServiceProvider implements ServiceProviderInterface
 {
     /**
-     * @throws ExceptionInterface
+     * @throws Throwable
      */
     public function __invoke(ContainerInterface $container): void
     {
-        $container->alias(ListenerProviderInterface::class, ListenerProvider::class);
         $container->alias(DispatcherInterface::class, Dispatcher::class);
-
-        /** @param ListenerProvider $listenerProvider */
-        $container->extend(
-            ListenerProvider::class,
-            static function (ContainerInterface $container, object $listenerProvider): ListenerProvider {
-                $finder = clone $container->get(Finder::class);
-
-                $finder->files()
-                    ->in(dirname(__DIR__) . '/Listener/')
-                    ->name('*Listener.php')
-                    ->notName('Abstract*.php')
-                    ->sortByName();
-
-                foreach ($finder->getIterator() as $splFileInfo) {
-                    /** @var class-string<EventInterface<bool>> $event */
-                    $event = sprintf(
-                        '%s%sEvent',
-                        str_replace('ServiceProvider', 'Event', __NAMESPACE__ . '\\'),
-                        $splFileInfo->getBasename('Listener.php')
-                    );
-
-                    /** @var callable-string $listener */
-                    $listener =  sprintf(
-                        "%s\%s",
-                        str_replace('ServiceProvider', 'Listener', __NAMESPACE__),
-                        $splFileInfo->getBasename('.php')
-                    );
-
-                    $listenerProvider->bindListener($event, $listener);
-                }
-
-                return $listenerProvider;
-            }
-        );
+        $container->alias(ListenerProviderInterface::class, ListenerProvider::class);
+        $container->extend(ListenerProvider::class, ListenerProviderExtension::class);
     }
 }
