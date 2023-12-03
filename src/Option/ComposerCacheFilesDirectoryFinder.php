5476f1c81c8cb3d69f8114fceac1405cc3944f0d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-12-03 12:00:08 -0600

    Update ComposerCacheFilesDirectoryFinder.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Option/ComposerCacheFilesDirectoryFinder.php
+++ src/Option/ComposerCacheFilesDirectoryFinder.php
@@ -5,17 +5,42 @@ declare(strict_types=1);
 namespace Ghostwriter\Compliance\Option;
 
 use Ghostwriter\Compliance\Service\Process;
+use function compact;
+use Ghostwriter\Compliance\Option\ComposerGlobalHomePathFinder;
 
 final readonly class ComposerCacheFilesDirectoryFinder
 {
     public function __construct(
-        private Process $process = new Process()
+        private Process $process = new Process(),
+        private ComposerExecutableFinder $composerExecutableFinder = new ComposerExecutableFinder(),
+        private ComposerGlobalHomePathFinder $composerGlobalHomePathFinder = new ComposerGlobalHomePathFinder(),
     ) {
     }
 
     public function find(): string
     {
-        [$stdout, $stderr] = $this->process->execute(['composer', 'config', 'cache-files-dir']);
+        $isWindowsOS = PHP_OS_FAMILY === 'Windows';
+
+        $composerExecutable = ($this->composerExecutableFinder)($this->process, $isWindowsOS);
+
+        $composerGlobalHomePath = ($this->composerGlobalHomePathFinder)($this->process, $composerExecutable);
+
+        $composerGlobalComposerJsonFilePath = sprintf(
+            '%s%scomposer.json',
+            $composerGlobalHomePath,
+            DIRECTORY_SEPARATOR
+        );
+
+        if ( ! file_exists($composerGlobalComposerJsonFilePath)) {
+            throw new \RuntimeException(sprintf(
+                'Could not find composer global composer.json file: %s',
+                $composerGlobalComposerJsonFilePath
+            ));
+        }
+
+        $command = [$composerExecutable, 'config', 'cache-files-dir'];
+
+        [$stdout, $stderr] = $this->process->execute($command);
 
         if (trim($stderr) !== '') {
             throw new \RuntimeException(sprintf(
@@ -23,7 +48,7 @@ final readonly class ComposerCacheFilesDirectoryFinder
                 PHP_EOL,
                 $stderr,
                 PHP_EOL,
-                $stdout 
+                $stdout
             ));
         }
 
