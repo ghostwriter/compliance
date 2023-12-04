9d593b504e85d1f5cb85040ed86135059dc6f87e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-12-04 04:03:43 -0600

    Update Finder.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Service/Finder.php
+++ src/Service/Finder.php
@@ -12,13 +12,10 @@ use SplFileInfo;
 final readonly class Finder
 {
     /**
-     * @param string $directory
      * @return Generator<SplFileInfo>
      */
     public function findIn(string $directory): Generator
     {
-        yield from new RecursiveIteratorIterator(
-        new RecursiveDirectoryIterator($directory)
-        );
+        yield from new RecursiveIteratorIterator(new RecursiveDirectoryIterator($directory));
     }
 }
