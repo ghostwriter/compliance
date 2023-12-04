62c4b56a23d08096885ff9f9dad05716aca7285f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-12-04 03:32:05 -0600

    Update MatrixCommand.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Command/MatrixCommand.php
+++ src/Command/MatrixCommand.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\Command;
 
-use Ghostwriter\Compliance\Compliance;
 use Ghostwriter\Compliance\Event\MatrixEvent;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputArgument;
