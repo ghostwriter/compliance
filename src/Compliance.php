69cedd80d47a48ba4342bdb45cea88e38fe25763
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2023-12-04 03:32:12 -0600

    Update Compliance.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Compliance.php
+++ src/Compliance.php
@@ -5,13 +5,10 @@ declare(strict_types=1);
 namespace Ghostwriter\Compliance;
 
 use Composer\InstalledVersions;
-use Ghostwriter\Compliance\Event\GitHubEvent;
 use Ghostwriter\Compliance\ServiceProvider\ApplicationServiceProvider;
 use Ghostwriter\Container\Container;
 use Ghostwriter\Environment\EnvironmentVariables;
-use Ghostwriter\EventDispatcher\Interface\DispatcherInterface;
 use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
