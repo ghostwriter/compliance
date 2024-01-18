41cae18dd17d766fd5690ba6a9a1d537811ff401
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-18 12:21:51 -0600

    Update CheckCommand.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Command/CheckCommand.php
+++ src/Command/CheckCommand.php
@@ -9,12 +9,8 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Throwable;
 
-#[AsCommand(
-    name: 'check',
-    description: 'Performs the QA job',
-)]
+#[AsCommand(name: 'check', description: 'Performs the QA job',)]
 final class CheckCommand extends AbstractCommand
 {
     protected function configure(): void
@@ -24,8 +20,6 @@ final class CheckCommand extends AbstractCommand
 
     /**
      * Execute the command.
-     *
-     * @throws Throwable
      *
      * @return int 0 if everything went fine, or an exit code
      */
