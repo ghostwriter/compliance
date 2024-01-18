e9593a9aa443112e7991446ec437ae301dd1194a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-18 10:23:15 -0600

    Update DependencyInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Interface/Composer/DependencyInterface.php
+++ src/Interface/Composer/DependencyInterface.php
@@ -2,15 +2,20 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\Compliance\Service\Composer;
+namespace Ghostwriter\Compliance\Interface\Composer;
 
+use Ghostwriter\Compliance\Service\Composer\DependencyName;
+use Ghostwriter\Compliance\Service\Composer\DependencyVersion;
 use JsonSerializable;
 use Stringable;
 
-interface Dependency extends JsonSerializable, Stringable
+interface DependencyInterface extends JsonSerializable, Stringable
 {
     public function __toString(): string;
+
     public function jsonSerialize(): array;
+
     public function name(): DependencyName;
+
     public function version(): DependencyVersion;
 }
