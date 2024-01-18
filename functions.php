2a4fbadb67aec39b75e042ed4f837b9cfc7022d1
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-18 12:23:22 -0600

    Update functions.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- functions.php
+++ functions.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 use Ghostwriter\Compliance\Compliance;
 use Ghostwriter\Compliance\Event\OutputEvent;
 use Ghostwriter\Compliance\Service\GithubActionOutput;
+use Ghostwriter\Compliance\Service\Process;
 use Ghostwriter\Container\Container;
 use Ghostwriter\Container\Interface\ContainerInterface;
 use Ghostwriter\EventDispatcher\Interface\DispatcherInterface;
 use Ghostwriter\EventDispatcher\Interface\EventInterface;
-use Ghostwriter\Compliance\Service\Process;
 
 function container(): ContainerInterface
 {
@@ -17,50 +17,44 @@ function container(): ContainerInterface
 }
 function dispatch(EventInterface $event): EventInterface
 {
-    return container()
+    return \container()
         ->get(DispatcherInterface::class)
         ->dispatch($event);
 }
-function dispatchOutputEvent(string|array $message): OutputEvent
+function dispatchOutputEvent(array|string $message): OutputEvent
 {
-   return dispatch(new OutputEvent(
-       [
-           '::echo::on',
-           sprintf('::group::%s %s', Compliance::NAME, Compliance::BLACK_LIVES_MATTER),
-           $message,
-           '::endgroup::',
-           '::echo::off'
-       ]
-   ));
+    return \dispatch(new OutputEvent(
+        [
+            '::echo::on',
+            \sprintf('::group::%s %s', Compliance::NAME, Compliance::BLACK_LIVES_MATTER),
+            $message,
+            '::endgroup::',
+            '::echo::off',
+        ]
+    ));
 }
 function githubActionOutput(): GithubActionOutput
 {
-   return container()
-       ->get(GithubActionOutput::class);
+    return \container()
+        ->get(GithubActionOutput::class);
 }
 function debug(string $message, ?string $file = null, ?int $line = null, ?int $col = null): void
 {
-    githubActionOutput()
+    \githubActionOutput()
         ->debug($message, $file, $line, $col);
 }
 function warning(string $message, ?string $file = null, ?int $line = null, ?int $col = null): void
 {
-    githubActionOutput()
+    \githubActionOutput()
         ->warning($message, $file, $line, $col);
 }
 function error(string $message, ?string $file = null, ?int $line = null, ?int $col = null): void
 {
-    githubActionOutput()
+    \githubActionOutput()
         ->error($message, $file, $line, $col);
 }
 
-function process(): Process
+function execute(string ...$command): array
 {
-    return container()
-       ->get(Process::class);
-}
-
-function runCmd(array $command): array
-{
-    return process()->execute($command);
+    return Process::execute($command);
 }
