284f34801c2031cbbbf9be1be31362f940db29da
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-18 14:01:49 -0600

    Update Logger.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Listener/Logger.php
+++ src/Listener/Logger.php
@@ -5,14 +5,19 @@ declare(strict_types=1);
 namespace Ghostwriter\Compliance\Listener;
 
 use Ghostwriter\Compliance\Event\GitHubEventInterface;
+use Ghostwriter\Compliance\Event\MatrixEvent;
 use Ghostwriter\Compliance\Interface\EventListenerInterface;
+use Ghostwriter\Container\Interface\ContainerInterface;
+use Ghostwriter\EventDispatcher\Interface\DispatcherInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use function sprintf;
 
 final readonly class Logger implements EventListenerInterface
 {
     public function __construct(
-        private SymfonyStyle $symfonyStyle
+        private ContainerInterface $container,
+        private DispatcherInterface $dispatcher,
+        private SymfonyStyle $symfonyStyle,
     ) {
     }
 
@@ -30,5 +35,7 @@ final readonly class Logger implements EventListenerInterface
             '<fg=white;bg=black;options=bold>Event Payload:</> <info>%s</info>',
             $event->payload()
         ));
+
+        $this->dispatcher->dispatch($this->container->get(MatrixEvent::class));
     }
 }
