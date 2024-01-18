73d3bbb32ffab8e0f2524fc4c9ff3b639a284cd0
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-18 12:17:19 -0600

    Update Extension.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Service/Composer/Extension.php
+++ src/Service/Composer/Extension.php
@@ -4,13 +4,27 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\Service\Composer;
 
+use Ghostwriter\Compliance\Interface\Composer\DependencyInterface;
+use function mb_substr;
 
-final readonly class Extension implements \JsonSerializable, \Stringable, Dependency
+final readonly class Extension implements DependencyInterface
 {
     public function __construct(
         private DependencyName $name,
         private DependencyVersion $version
     ) {
+    }
+
+    public function __toString(): string
+    {
+        return mb_substr((string) $this->name, 4);
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            (string) $this => (string) $this->version,
+        ];
     }
 
     public function name(): DependencyName
@@ -23,17 +37,8 @@ final readonly class Extension implements \JsonSerializable, \Stringable, Depend
         return $this->version;
     }
 
-    public function __toString(): string
+    public static function new(DependencyName $dependencyName, DependencyVersion $dependencyVersion): self
     {
-        return mb_substr((string) $this->name, 4);
-    }
-
-    public function jsonSerialize(): array
-    {
-        return [ (string)  $this => (string) $this->version];
+        return new self($dependencyName, $dependencyVersion);
     }
 }
-
-
-
-
