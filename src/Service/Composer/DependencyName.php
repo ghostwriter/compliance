95b475f067c57f660af953d844c277da4a5721c0
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-18 12:17:30 -0600

    Update DependencyName.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Service/Composer/DependencyName.php
+++ src/Service/Composer/DependencyName.php
@@ -4,13 +4,19 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\Service\Composer;
 
-final readonly class DependencyName implements \JsonSerializable, \Stringable
+use InvalidArgumentException;
+use JsonSerializable;
+use Stringable;
+use function str_starts_with;
+use function trim;
+
+final readonly class DependencyName implements JsonSerializable, Stringable
 {
     public function __construct(
         private string $content
     ) {
         if (trim($content) === '') {
-            throw new \InvalidArgumentException('Name cannot be empty');
+            throw new InvalidArgumentException('Name cannot be empty');
         }
     }
 
@@ -19,15 +25,18 @@ final readonly class DependencyName implements \JsonSerializable, \Stringable
         return $this->content;
     }
 
+    public function isPhpExtension(): bool
+    {
+        return str_starts_with($this->content, 'ext-');
+    }
+
     public function jsonSerialize(): array
     {
         return [$this->content];
     }
 
-    public function isPhpExtension(): bool
+    public static function new(string $name): self
     {
-        return str_starts_with($this->content, 'ext-');
+        return new self($name);
     }
 }
-
-
