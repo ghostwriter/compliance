8c5a489def50319fb732084cf830b06a6518fd1f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-18 12:18:45 -0600

    Update MatrixListener.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Listener/MatrixListener.php
+++ src/Listener/MatrixListener.php
@@ -5,43 +5,45 @@ declare(strict_types=1);
 namespace Ghostwriter\Compliance\Listener;
 
 use Composer\Semver\Semver;
-use Ghostwriter\Compliance\Contract\EventListenerInterface;
+use Ghostwriter\Compliance\Enum\ComposerDependency;
+use Ghostwriter\Compliance\Enum\OperatingSystem;
+use Ghostwriter\Compliance\Enum\PhpVersion;
+use Ghostwriter\Compliance\EnvironmentVariables;
 use Ghostwriter\Compliance\Event\MatrixEvent;
-use Ghostwriter\Compliance\Option\ComposerDependency;
-use Ghostwriter\Compliance\Option\Job;
-use Ghostwriter\Compliance\Option\PhpVersion;
+use Ghostwriter\Compliance\Interface\EventListenerInterface;
 use Ghostwriter\Compliance\Service\Composer;
+use Ghostwriter\Compliance\Service\Composer\Extension;
+use Ghostwriter\Compliance\Service\ComposerCacheFilesDirectoryFinder;
+use Ghostwriter\Compliance\Service\Job;
+use Ghostwriter\Compliance\Tool\PHPUnit;
 use Ghostwriter\Compliance\ToolInterface;
 use Ghostwriter\Container\Interface\ContainerInterface;
 use RuntimeException;
 use Symfony\Component\Console\Command\Command;
-use Throwable;
+use const FILE_APPEND;
+use const PHP_EOL;
+use function array_map;
+use function array_unique;
+use function chdir;
+use function dispatchOutputEvent;
+use function file_put_contents;
 use function getcwd;
-use Ghostwriter\Compliance\Service\Composer\Extension;
-use Ghostwriter\Compliance\Tool\PHPUnit;
-use Ghostwriter\Compliance\Option\ComposerCacheFilesDirectoryFinder;
-use Ghostwriter\Environment\EnvironmentVariables;
+use function iterator_to_array;
+use function sprintf;
+use function sys_get_temp_dir;
+use function tempnam;
 
 final readonly class MatrixListener implements EventListenerInterface
 {
-    /**
-     * @var string[]
-     */
-    private const DEPENDENCIES = ['highest', 'locked', 'lowest'];
-
     public function __construct(
         // remove the damn container im using it to get the tools that were tagged
         private ContainerInterface $container,
-        private Composer           $composer,
+        private Composer $composer,
         private ComposerCacheFilesDirectoryFinder $composerCacheFilesDirectoryFinder,
         private EnvironmentVariables $environmentVariables,
-    )
-    {
+    ) {
     }
 
-    /**
-     * @throws Throwable
-     */
     public function __invoke(MatrixEvent $generateMatrixEvent): void
     {
         $root = getcwd();
@@ -55,7 +57,7 @@ final readonly class MatrixListener implements EventListenerInterface
         $composerCacheFilesDirectory = '/home/runner/.cache/composer/files';
 
         //TODO: fix this ^^^^^
-        
+
         // print_r([
         //     $composerCacheFilesDirectory,
         //     ($this->composerCacheFilesDirectoryFinder)()
@@ -72,61 +74,60 @@ final readonly class MatrixListener implements EventListenerInterface
         $constraints = $phpVersionConstraint->getVersion();
 
         $requiredPhpExtensions = array_map(
-            fn (Extension $extension) => (string) $extension,
+            static fn (Extension $extension) => (string) $extension,
             iterator_to_array($composerJson->getRequiredPhpExtensions())
         );
 
-        $oss = ['macos','ubuntu','windows'];
-
         foreach ($this->container->tagged(ToolInterface::class) as $tool) {
-            if (!$tool->isPresent()) {
+            if (! $tool->isPresent()) {
                 continue;
             }
 
             $name = $tool->name();
+
             $command = $tool->command();
-            $extensions = $tool->extensions();
+
+            $extensions = array_unique([...$requiredPhpExtensions, ...$tool->extensions()]);
 
             if (! $tool instanceof PHPUnit) {
                 $generateMatrixEvent->include(
-                    new Job(
+                    Job::new(
                         $name,
                         $command,
-                        [...$requiredPhpExtensions, ...$extensions],
+                        $extensions,
                         $composerCacheFilesDirectory,
                         $composerJsonPath,
                         $composerLockPath,
-                        'locked',
-                        PhpVersion::LATEST
+                        PhpVersion::latest(),
                     )
                 );
                 continue;
             }
 
-            foreach (PhpVersion::SUPPORTED as $phpVersion) {
-                if (!Semver::satisfies(PhpVersion::TO_STRING[$phpVersion], $constraints)) {
+            foreach (PhpVersion::cases() as $phpVersion) {
+                if (! Semver::satisfies($phpVersion->toString(), $constraints)) {
                     continue;
                 }
 
-                $isExperimental = $phpVersion >= PhpVersion::DEV;
+                $isPhpVersionExperimental = PhpVersion::isExperimental($phpVersion);
 
-                foreach (ComposerDependency::SUPPORTED as $dependency) {
-                    
-                    $isExperimental = $isExperimental || $dependency === ComposerDependency::LOWEST;
+                foreach (ComposerDependency::cases() as $composerDependency) {
+                    $isComposerDependencyExperimental = $isPhpVersionExperimental
+                        || ComposerDependency::isExperimental($composerDependency);
 
-                    foreach($oss as $os){
+                    foreach (OperatingSystem::cases() as $operatingSystem) {
                         $generateMatrixEvent->include(
-                            new Job(
+                            Job::new(
                                 $name,
                                 $command,
-                                [...$requiredPhpExtensions, ...$extensions],
+                                $extensions,
                                 $composerCacheFilesDirectory,
                                 $composerJsonPath,
                                 $composerLockPath,
-                                $dependency,
                                 $phpVersion,
-                                $isExperimental,
-                                $os
+                                $composerDependency,
+                                $operatingSystem,
+                                $isComposerDependencyExperimental,
                             )
                         );
                     }
@@ -134,22 +135,15 @@ final readonly class MatrixListener implements EventListenerInterface
             }
         }
 
-        $gitHubOutput = $this->environmentVariables->get('GITHUB_OUTPUT', tempnam(sys_get_temp_dir(),'GITHUB_OUTPUT'));
+        $gitHubOutput = $this->environmentVariables->get('GITHUB_OUTPUT', tempnam(sys_get_temp_dir(), 'GITHUB_OUTPUT'));
 
         $matrix = sprintf('matrix=%s' . PHP_EOL, $generateMatrixEvent->getMatrix());
 
-        file_put_contents(
-            $gitHubOutput,
-            $matrix,
-            FILE_APPEND
-        );
+        file_put_contents($gitHubOutput, $matrix, FILE_APPEND);
 
         dispatchOutputEvent($matrix);
     }
 
-    /**
-     * @throws Throwable
-     */
     public function write(string $message): int
     {
         return dispatchOutputEvent($message)->isPropagationStopped() ? Command::FAILURE : Command::SUCCESS;
