67057a8ab849099d6172bd6ba8d84c4325536422
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-18 11:07:46 -0600

    Update GitHubPublicEvent.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Event/GitHub/GitHubPublicEvent.php
+++ src/Event/GitHub/GitHubPublicEvent.php
@@ -4,14 +4,20 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\Event\GitHub;
 
-use Ghostwriter\EventDispatcher\Interface\EventInterface;
+use Ghostwriter\Compliance\Event\GitHubEventInterface;
 use Ghostwriter\EventDispatcher\Trait\EventTrait;
 
-final class GitHubPublicEvent implements EventInterface
+final class GitHubPublicEvent implements GitHubEventInterface
 {
     use EventTrait;
 
-    public function __construct(private string $content)
+    public function __construct(
+        private string $content
+    ) {
+    }
+
+    public function payload(): string
     {
+        return $this->content;
     }
 }
