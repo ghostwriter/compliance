5e26eaa81f2c2ac1ed6678e9de4068dc90b47373
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-18 12:17:34 -0600

    Update ComposerLockReader.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Service/Composer/ComposerLockReader.php
+++ src/Service/Composer/ComposerLockReader.php
@@ -5,21 +5,22 @@ declare(strict_types=1);
 namespace Ghostwriter\Compliance\Service\Composer;
 
 use Ghostwriter\Json\Json;
+use InvalidArgumentException;
+use function file_exists;
+use function file_get_contents;
 
-final readonly class ComposerLockReader 
+final readonly class ComposerLockReader
 {
-    public function read(
-        string $composerJsonPath
-    ): ComposerLock
+    public function read(string $composerJsonPath): ComposerLock
     {
-        if (!file_exists($composerJsonPath)) {
-            throw new \InvalidArgumentException('Composer JSON file does not exist');
+        if (! file_exists($composerJsonPath)) {
+            throw new InvalidArgumentException('Composer JSON file does not exist');
         }
 
         $composerJsonContents = file_get_contents($composerJsonPath);
 
-        if ($composerJsonContents === false){
-            throw new \InvalidArgumentException('Composer JSON file could not be read');
+        if ($composerJsonContents === false) {
+            throw new InvalidArgumentException('Composer JSON file could not be read');
         }
 
         return new ComposerLock($composerJsonPath, Json::decode($composerJsonContents));
