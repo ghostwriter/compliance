ea31d8c8322a8c4b64c1a303dc4b22f700f5f626
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-18 12:17:28 -0600

    Update DependencyVersion.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Service/Composer/DependencyVersion.php
+++ src/Service/Composer/DependencyVersion.php
@@ -4,13 +4,18 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\Service\Composer;
 
-final readonly class DependencyVersion implements \JsonSerializable, \Stringable
+use InvalidArgumentException;
+use JsonSerializable;
+use Stringable;
+use function trim;
+
+final readonly class DependencyVersion implements JsonSerializable, Stringable
 {
     public function __construct(
         private string $content
     ) {
         if (trim($content) === '') {
-            throw new \InvalidArgumentException('Version cannot be empty');
+            throw new InvalidArgumentException('Version cannot be empty');
         }
     }
 
@@ -23,6 +28,9 @@ final readonly class DependencyVersion implements \JsonSerializable, \Stringable
     {
         return [$this->content];
     }
+
+    public static function new(string $content): self
+    {
+        return new self($content);
+    }
 }
-
-
