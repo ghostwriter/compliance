670d9e117e98f44e77234e8cd3c5d96835308788
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-18 12:26:01 -0600

    Update PhpVersionConstraintInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Service/PhpVersionConstraintInterface.php
+++ src/Service/PhpVersionConstraintInterface.php
@@ -4,14 +4,11 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\Service;
 
-interface PhpVersionConstraint
+interface PhpVersionConstraintInterface
 {
     public const LATEST = 'latest';
 
-    public const SUPPORTED = [
-        '7.4',
-        '8.0',
-        '8.1',
-    ];
+    public const SUPPORTED = ['7.4', '8.0', '8.1'];
+
     public function getVersion(): string;
 }
