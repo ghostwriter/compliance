eebffb0ba35b18e1df9de4923168fe8c6e8fcb12
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-18 12:17:43 -0600

    Update ComposerJsonReader.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Service/Composer/ComposerJsonReader.php
+++ src/Service/Composer/ComposerJsonReader.php
@@ -5,24 +5,22 @@ declare(strict_types=1);
 namespace Ghostwriter\Compliance\Service\Composer;
 
 use Ghostwriter\Json\Json;
-use Ghostwriter\Compliance\Service\Composer\ComposerJson;
+use InvalidArgumentException;
+use function file_exists;
+use function file_get_contents;
 
-final readonly class ComposerJsonReader 
+final readonly class ComposerJsonReader
 {
-    public function read(
-        string $composerJsonPath
-    ): ComposerJson
+    public function read(string $composerJsonPath): ComposerJson
     {
-        if (!file_exists($composerJsonPath)) {
-            throw new \InvalidArgumentException(
-                'Composer JSON file does not exist: ' . $composerJsonPath
-            );
+        if (! file_exists($composerJsonPath)) {
+            throw new InvalidArgumentException('Composer JSON file does not exist: ' . $composerJsonPath);
         }
 
         $composerJsonContents = file_get_contents($composerJsonPath);
 
-        if ($composerJsonContents === false){
-            throw new \InvalidArgumentException('Composer JSON file could not be read');
+        if ($composerJsonContents === false) {
+            throw new InvalidArgumentException('Composer JSON file could not be read');
         }
 
         return new ComposerJson($composerJsonPath, Json::decode($composerJsonContents));
