1706054f37e2872cb603c765a84a5bd7d11c4019
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-18 12:16:24 -0600

    Update License.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Service/Composer/License.php
+++ src/Service/Composer/License.php
@@ -4,14 +4,16 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\Service\Composer;
 
-final readonly class License implements \JsonSerializable, \Stringable
+use InvalidArgumentException;
+use JsonSerializable;
+use Stringable;
+use function trim;
+
+final readonly class License implements JsonSerializable, Stringable
 {
     public function __construct(
         private string $content
     ) {
-        if (trim($content) === '') {
-            throw new \InvalidArgumentException('Version cannot be empty');
-        }
     }
 
     public function __toString(): string
@@ -23,6 +25,13 @@ final readonly class License implements \JsonSerializable, \Stringable
     {
         return [$this->content];
     }
+
+    public static function new(?string $content): self
+    {
+        return match (true) {
+            $content === null => throw new InvalidArgumentException('License cannot be null'),
+            trim($content) === '' => throw new InvalidArgumentException('License cannot be empty'),
+            default => new self($content)
+        };
+    }
 }
-
-
