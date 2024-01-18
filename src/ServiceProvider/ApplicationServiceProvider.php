c6b8e678ed64e74b687734aac54c227d782a6c84
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-18 12:01:22 -0600

    Update ApplicationServiceProvider.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/ServiceProvider/ApplicationServiceProvider.php
+++ src/ServiceProvider/ApplicationServiceProvider.php
@@ -4,23 +4,82 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\ServiceProvider;
 
+use Ghostwriter\Compliance\EnvironmentVariables;
+use Ghostwriter\Compliance\Factory\ComposerExecutableFinderFactory;
+use Ghostwriter\Compliance\Service\ComposerExecutableFinder;
+use Ghostwriter\Compliance\Service\Filesystem;
+use Ghostwriter\Compliance\ToolInterface;
 use Ghostwriter\Container\Interface\ContainerInterface;
 use Ghostwriter\Container\Interface\ServiceProviderInterface;
-use Throwable;
-use Ghostwriter\Compliance\Option\ComposerExecutableFinder;
-use Ghostwriter\Compliance\Factory\ComposerExecutableFinderFactory;
+use const DIRECTORY_SEPARATOR;
+use function dirname;
+use function is_a;
+use function sprintf;
+use function str_contains;
+use function str_ends_with;
+use function str_replace;
 
 final readonly class ApplicationServiceProvider implements ServiceProviderInterface
 {
-    /**
-     * @throws Throwable
-     */
+    public function __construct(
+        private Filesystem $filesystem,
+    ) {
+    }
+
     public function __invoke(ContainerInterface $container): void
     {
-        $container->provide(ConsoleServiceProvider::class);
-        $container->provide(EventServiceProvider::class);
-        $container->provide(ConfigServiceProvider::class);
-        $container->provide(MatrixServiceProvider::class);
+        $this->registerServiceProviders($container);
+        $this->registerTools($container);
+
+        $container->set(EnvironmentVariables::class, EnvironmentVariables::new());
         $container->factory(ComposerExecutableFinder::class, ComposerExecutableFinderFactory::class);
+    }
+
+    private function registerServiceProviders(ContainerInterface $container): void
+    {
+        // Autoload ServiceProvider classes.
+        foreach ($this->filesystem->findIn(__DIR__) as $file) {
+            $path = $file->getPathname();
+
+            if (str_contains($path, 'Abstract') || ! str_ends_with($path, 'ServiceProvider.php')) {
+                continue;
+            }
+
+            $class = sprintf('%s\%s', __NAMESPACE__, $file->getBasename('.php'));
+
+            if ($class === self::class) {
+                continue;
+            }
+
+            if (! is_a($class, ServiceProviderInterface::class, true)) {
+                continue;
+            }
+
+            $container->provide($class);
+        }
+    }
+
+    private function registerTools(ContainerInterface $container): void
+    {
+        // Tag the CLI tools we support.
+        foreach ($this->filesystem->findIn(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'Tool') as $file) {
+            $path = $file->getPathname();
+
+            if (str_contains($path, 'Abstract') || ! str_ends_with($path, '.php')) {
+                continue;
+            }
+
+            $service = sprintf(
+                '%s%s',
+                str_replace('ServiceProvider', 'Tool', __NAMESPACE__ . '\\'),
+                $file->getBasename('.php')
+            );
+
+            if (! is_a($service, ToolInterface::class, true)) {
+                continue;
+            }
+
+            $container->tag($service, [ToolInterface::class]);
+        }
     }
 }
