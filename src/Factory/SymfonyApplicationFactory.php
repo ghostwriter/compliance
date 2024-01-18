266985ae8017aa1459e3ceec3a932857048b3055
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-18 12:21:28 -0600

    Update SymfonyApplicationFactory.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Factory/SymfonyApplicationFactory.php
+++ src/Factory/SymfonyApplicationFactory.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Ghostwriter\Compliance\Factory;
 
 use Composer\InstalledVersions;
@@ -8,34 +10,29 @@ use Ghostwriter\Container\Interface\ContainerInterface;
 use Ghostwriter\Container\Interface\FactoryInterface;
 use RuntimeException;
 use Symfony\Component\Console\Application as SymfonyApplication;
-use Throwable;
 
 /**
  * @implements FactoryInterface<SymfonyApplication>
  */
 final readonly class SymfonyApplicationFactory implements FactoryInterface
 {
-    /**
-     * @throws Throwable
-     */
     public function __invoke(ContainerInterface $container, array $arguments = []): SymfonyApplication
     {
-//        $config = $container->get(ConfigInterface::class);
-// later we'll connect the config file, lets first get the app running
+        //        $config = $container->get(ConfigInterface::class);
+        // later we'll connect the config file, lets first get the app running
 
-//        $parameters = [
-//            $config->get('app.name', Compliance::NAME),
-//            $config->get('app.version',
-//
-//            ), ...$arguments
-//        ];
+        //        $parameters = [
+        //            $config->get('app.name', Compliance::NAME),
+        //            $config->get('app.version',
+        //
+        //            ), ...$arguments
+        //        ];
         // Woot! This works!
 
-
-//        Lets extract the factory implementation from the vendor directory into the actual project.
+        //        Lets extract the factory implementation from the vendor directory into the actual project.
         return new SymfonyApplication(
             Compliance::NAME,
-                InstalledVersions::getPrettyVersion(Compliance::PACKAGE) ??
+            InstalledVersions::getPrettyVersion(Compliance::PACKAGE) ??
             throw new RuntimeException('Unable to determine version!')
         );
     }
