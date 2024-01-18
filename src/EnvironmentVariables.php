f509cc91594d7de73894898dbd04143521f16f4b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-18 11:04:49 -0600

    Create EnvironmentVariables.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/EnvironmentVariables.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Compliance;
+
+use Ghostwriter\Compliance\Exception\VariableNotFoundException;
+use function array_key_exists;
+
+final readonly class EnvironmentVariables
+{
+    private function __construct(
+        private array $variables = [],
+    ) {
+    }
+
+    public function get(string $name, null|string $default = null): string
+    {
+        return match (true) {
+            array_key_exists($name, $this->variables) => $this->variables[$name],
+            $default !== null => $default,
+            default => throw new VariableNotFoundException($name),
+        };
+    }
+
+    public function has(string $name): bool
+    {
+        return array_key_exists($name, $this->variables);
+    }
+
+    public static function new(): self
+    {
+        $variables = [];
+
+        foreach ($_ENV as $name => $value) {
+            $variables[$name] = (string) $value;
+        }
+
+        return new self($variables);
+    }
+}
