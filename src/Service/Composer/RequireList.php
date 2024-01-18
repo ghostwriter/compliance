2579f89217023c27df6745b2831d1c4a196c3249
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-18 12:15:28 -0600

    Update RequireList.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Service/Composer/RequireList.php
+++ src/Service/Composer/RequireList.php
@@ -13,15 +13,31 @@ use IteratorAggregate;
 final readonly class RequireList implements IteratorAggregate
 {
     /**
-     * @param array<Package|Extension> $list
+     * @param array<Extension|Package> $requireList
      */
     public function __construct(
-        private array $list,
+        private array $requireList,
     ) {
     }
 
     public function getIterator(): Generator
     {
-        yield from $this->list;
+        yield from $this->requireList;
+    }
+
+    public static function new(array $require): self
+    {
+        $requireList = [];
+
+        foreach ($require as $name => $version) {
+            $dependencyName = DependencyName::new($name);
+            $dependencyVersion = DependencyVersion::new($version);
+
+            $requireList[$name] = $dependencyName->isPhpExtension()
+                ? Extension::new($dependencyName, $dependencyVersion)
+                : Package::new($dependencyName, $dependencyVersion);
+        }
+
+        return new self($requireList);
     }
 }
