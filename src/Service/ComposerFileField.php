adcec47f82a408e6b65671e864b1c0b0f07625d3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-18 12:15:54 -0600

    Update ComposerFileField.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Service/ComposerFileField.php
+++ src/Service/ComposerFileField.php
@@ -4,27 +4,30 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\Service;
 
-
 final readonly class ComposerFileField
 {
     /**
      * @var string
      */
-    public const NAME = 'name';
-    /**
-     * @var string
-     */
     public const DESCRIPTION = 'description';
-    /**
-     * @var string
-     */
-    public const VERSION = 'version';
+
     /**
      * @var string
      */
     public const LICENSE = 'license';
+
+    /**
+     * @var string
+     */
+    public const NAME = 'name';
+
     /**
      * @var string
      */
     public const REQUIRE = 'require';
+
+    /**
+     * @var string
+     */
+    public const VERSION = 'version';
 }
