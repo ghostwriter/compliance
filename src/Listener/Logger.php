5299ca8be35f8e959da4110e4d763a281604b75a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-01-18 18:16:02 -0600

    Update Logger.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Listener/Logger.php
+++ src/Listener/Logger.php
@@ -4,13 +4,18 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Compliance\Listener;
 
+use Ghostwriter\Compliance\Event\GitHub\GitHubPullRequestEvent;
+use Ghostwriter\Compliance\Event\GitHub\GitHubPushEvent;
+use Ghostwriter\Compliance\Event\GitHub\GitHubScheduleEvent;
+use Ghostwriter\Compliance\Event\GitHub\GitHubWorkflowCallEvent;
+use Ghostwriter\Compliance\Event\GitHub\GitHubWorkflowDispatchEvent;
+use Ghostwriter\Compliance\Event\GitHub\GitHubWorkflowRunEvent;
 use Ghostwriter\Compliance\Event\GitHubEventInterface;
 use Ghostwriter\Compliance\Event\MatrixEvent;
 use Ghostwriter\Compliance\Interface\EventListenerInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use function container;
 use function dispatch;
-use function sprintf;
 
 final readonly class Logger implements EventListenerInterface
 {
@@ -24,8 +29,24 @@ final readonly class Logger implements EventListenerInterface
      */
     public function __invoke(GitHubEventInterface $event): void
     {
-        $this->symfonyStyle->info('Event Class:</> <info>'.$event::class.'</info>');
-        $this->symfonyStyle->info('Event Payload: '.$event->payload());
+        $this->symfonyStyle->info('Event Class: ' . $event::class);
+        $this->symfonyStyle->info('Event Payload: ' . $event->payload());
+
+        $stop = match (true) {
+//            $event instanceof GitHubPullRequestEvent,
+            $event instanceof GitHubWorkflowCallEvent,
+            $event instanceof GitHubWorkflowDispatchEvent,
+            $event instanceof GitHubScheduleEvent,
+            $event instanceof GitHubWorkflowRunEvent,
+            $event instanceof GitHubPushEvent => false,
+            default => true,
+        };
+
+        if ($stop) {
+            return;
+        }
+
+        dispatch(container()->get(MatrixEvent::class));
 
 
 //        $this->symfonyStyle->info(sprintf(
@@ -37,7 +58,5 @@ final readonly class Logger implements EventListenerInterface
 //            '<fg=white;bg=black;options=bold>Event Payload:</> <info>%s</info>',
 //            $event->payload()
 //        ));
-
-        dispatch(container()->get(MatrixEvent::class));
     }
 }
