26490a86374d7d89d3ad23cbb90e4ec64daf5c77
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-03-10 12:00:44 -0500

    Update GitHubMilestoneEvent.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Event/GitHub/GitHubMilestoneEvent.php
+++ src/Event/GitHub/GitHubMilestoneEvent.php
@@ -7,8 +7,14 @@ namespace Ghostwriter\Compliance\Event\GitHub;
 use Ghostwriter\Compliance\Event\GitHubEventInterface;
 use Ghostwriter\EventDispatcher\Trait\EventTrait;
 
+/**
+ * @template TStopped of bool
+ *
+ * @implements GitHubEventInterface<TStopped>
+ */
 final class GitHubMilestoneEvent implements GitHubEventInterface
 {
+    /** @use EventTrait<TStopped> */
     use EventTrait;
 
     public function __construct(
