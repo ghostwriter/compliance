225f2ccc399f79615f10a181665537c68fe350c3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-08-23 20:18:39 -0500

    Add PHP version 8.5 to supported versions
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Value/Composer/PhpVersionConstraintInterface.php
+++ src/Value/Composer/PhpVersionConstraintInterface.php
@@ -6,7 +6,7 @@ namespace Ghostwriter\Compliance\Value\Composer;
 
 interface PhpVersionConstraintInterface
 {
-    public const array SUPPORTED = ['7.4', '8.0', '8.1', '8.2', '8.3', '8.4'];
+    public const array SUPPORTED = ['7.4', '8.0', '8.1', '8.2', '8.3', '8.4', '8.5'];
 
     public const string LATEST = 'latest';
 
