933ac2955022b5eafc81b9223878d85de11c9056
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-12-05 20:27:31 -0600

    Update Infection.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Tool/Infection.php
+++ src/Tool/Infection.php
@@ -11,7 +11,7 @@ final class Infection extends AbstractTool
     #[Override]
     public function command(): string
     {
-        return 'composer infection';
+        return 'infection';
     }
 
     /** @return list<string> */
